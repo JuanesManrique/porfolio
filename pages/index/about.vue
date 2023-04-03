3de2e3954e82c41092ca6db5d859bb3a31f99e42
JuanManrique <juansanchezmanrique@gmail.com>
2023-04-02 21:24:30 -0500

    about finalizado

--- pages/index/about.vue
+++ pages/index/about.vue
@@ -2,7 +2,7 @@
   <div class="about__container py-20 w-full h-full flex justify-between items-center gap-20">
     <div class="shadow__cards p-10 w-1/2 h-full flex flex-col justify-center rounded-2xl" style="background-color: var(--negro);">
       <p class="mb-5 text-3xl font-black">Juan Esteban Sanchez Manrique</p>
-      <p class="mb-3 text-lg">Soy un programador Front-end Developer con experiencia en desarrollos web con Vue.js empleando habilidades de estilos con tailwind, pruebas con Cypress.io, API REST y demas tecnologias complementarias al desarrollo front-end...</p>
+      <p class="mb-3 text-lg">Soy un programador Front-end Developer con experiencia en desarrollos web con Vue.js empleando habilidades de estilos con tailwind, pruebas con Cypress.io, API REST y demas tecnologias complementarias al desarrollo front-end.</p>
       <p class="text-lg">Además de ser programador tambien entreno Capoeira, un arte marcial afro-brasileña en el cual llevo 11 años donde he participado en diversos eventos y campeonatos nacionales con el grupo Capoeira Nativos.</p>
     </div>
     <!-- <img src="../../assets/foto.jpg" alt="" class="shadow__cards img__about w-1/2 h-full rounded-2xl"> -->
