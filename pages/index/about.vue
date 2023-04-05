ff51e58f6ae32f13c14a4e3c505e7e1535b5ec9a
JuanManrique <juansanchezmanrique@gmail.com>
2023-04-05 00:13:25 -0500

    correccion texto about

--- pages/index/about.vue
+++ pages/index/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about__container px-5 xl:px-0 pb-12 md:py-20 w-full h-full flex flex-col md:flex-row items-center gap-10 lg:gap-20">
     <div class="shadow__cards p-10 w-full md:w-1/2 h-1/2 md:h-full  flex flex-col justify-center rounded-2xl" style="background-color: var(--negro);">
-      <p class="mb-5 text-xl lg:text-3xl text-center md:text-start font-black">Juan Esteban Sanchez Manrique</p>
+      <p class="mb-5 text-xl lg:text-3xl text-start font-black">Juan Esteban Sanchez Manrique</p>
       <p class="mb-3 text-sm lg:text-lg">Soy un programador Front-end Developer con experiencia en desarrollos web con Vue.js empleando habilidades de estilos con tailwind, pruebas con Cypress.io, API REST y demas tecnologias complementarias al desarrollo front-end.</p>
       <p class="text-sm  lg:text-lg">Además de ser programador tambien entreno Capoeira, un arte marcial afro-brasileña en el cual llevo 11 años donde he participado en diversos eventos y campeonatos nacionales con el grupo Capoeira Nativos.</p>
     </div>
